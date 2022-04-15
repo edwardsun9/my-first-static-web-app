0b2f5cf5959c611e25c9c1f3b5f93028628e4730
Edward <pedwardsun@gmail.com>
2022-04-15 16:49:15 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   template: `<div>Hello {{value}}</div>`,
 })
 export class AppComponent {
-  value = 'World';
+  value = '';
+
+  constructor(private http: HttpClient) {
+    this.http.get('/api/Message')
+      .subscribe((resp: any) => this.value = resp.text);
+  }
 }
